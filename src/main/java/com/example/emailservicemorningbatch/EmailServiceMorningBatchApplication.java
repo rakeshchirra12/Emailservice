9bb73f6c2355b1f881888dc0513f21fe4843f43d
rakeshchirra12 <rakeshch@Rakeshs-MacBook-Air.local>
2025-03-27 23:25:58 +0530

    inital

---
+++ src/main/java/com/example/emailservicemorningbatch/EmailServiceMorningBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailservicemorningbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceMorningBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServiceMorningBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emailservicemorningbatch/EmailServiceMorningBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailservicemorningbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceMorningBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
